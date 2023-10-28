c4b7682059d65263bb5d6cc12ccaf80ccd1fcd0c
Deepak Sharma <sharmadeepak0939@gmail.com>
2023-10-28 20:58:27 +0530

    Initialize project using Create React App

---
+++ loginPage.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './LoginPage.css';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // You can add your login logic here, e.g., send the data to a server or check against a predefined username and password.
+
+    console.log('Submitted:', { username, password });
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-form">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
